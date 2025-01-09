6c610e03b9d083f29c5c60e6cba1443917fe6c44
Amit Gaikwad <amitmanojgaikwad@gmail.com>
2025-01-09 14:23:29 +0530

    init

---
+++ api/api.go
@@ -0,0 +1,17 @@
+package api
+
+type CoinBalanceParams struct {
+	Username string
+}
+
+type CoinBalanceResponse struct {
+	Code int
+
+	Balance int64
+}
+
+type Error struct {
+	Code int
+
+	Message string
+}
---
+++ cmd/api/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/go-chi/chi"
+	// "github.com/avukadin/goapi/internal/handlers"
+	"github.com/ameeetgaikwad/go-crud-program/internal/handlers"
+	log "github.com/sirupsen/logrus"
+)
+
+func main(){
+
+}
